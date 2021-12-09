7a2db432a982787859ee73e7819ee6928a3d96a1
Andrew Pearson <crazycan@umich.edu>
2021-12-09 11:52:01 -0500

    Commit

---
+++ testSelectionSort.java
@@ -0,0 +1,19 @@
+/*
+ * testSelectionSort.java
+ *
+* To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Test;
+public class testSelectionSort {
+@Test
+public void test() {
+
+}
+    public testSelectionSort() {
+    }
+
+
+}
